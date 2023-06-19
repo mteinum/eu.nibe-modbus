2b9eec809bb1617890ff6a62fd37841ff9fc7729
x-erik <faassene@gmail.com>
2023-06-19 08:30:26 +0200

    Added Compressor Frequency Added BT7 Hot Water Top Changed BT7 from Hot Water to Hot Water Load

--- drivers/s1155pc/device.js
+++ drivers/s1155pc/device.js
@@ -78,20 +78,22 @@ class s11556pcDevice extends Device {
         Promise.all([
 
             // Read all modbus register we want
-            client.readInputRegisters(1, 1),    // BT1 - outside temperature
-            client.readInputRegisters(37, 1),   // BT1 - Average outside temperature
-            client.readInputRegisters(10, 1),   // BT10 - ground source medium IN
-            client.readInputRegisters(11, 1),   // BT11 - ground source medium OUT
-            client.readInputRegisters(5, 1),    // BT2 - heating supply
-            client.readInputRegisters(7, 1),    // BT3 - heating return
-            client.readHoldingRegisters(18, 1), // Degree minutes ==> Holding register
-            client.readInputRegisters(9, 1),    // BT6 - Hot water
-            client.readInputRegisters(1017, 1), // BT2 - calculated supply
-            client.readInputRegisters(1102, 1), // Heating pump % 
-            client.readInputRegisters(1104, 1), // Source pump %
-            client.readInputRegisters(1028, 1),  // Priority
-            client.readInputRegisters(116, 1),   // RMU S40 temperature
-            client.readInputRegisters(2166, 1)   // Energy usage
+            client.readInputRegisters(1, 1),      // BT1 - outside temperature
+            client.readInputRegisters(37, 1),     // BT1 - Average outside temperature
+            client.readInputRegisters(10, 1),     // BT10 - ground source medium IN
+            client.readInputRegisters(11, 1),     // BT11 - ground source medium OUT
+            client.readInputRegisters(5, 1),      // BT2 - heating supply
+            client.readInputRegisters(7, 1),      // BT3 - heating return
+            client.readHoldingRegisters(18, 1),   // Degree minutes ==> Holding register
+            client.readInputRegisters(8, 1),      // BT7 - Hot water top
+            client.readInputRegisters(9, 1),      // BT6 - Hot water load
+            client.readInputRegisters(1017, 1),   // BT2 - calculated supply
+            client.readInputRegisters(1102, 1),   // Heating pump % 
+            client.readInputRegisters(1104, 1),   // Source pump %
+            client.readInputRegisters(1028, 1),   // Priority
+            client.readInputRegisters(116, 1),    // RMU S40 temperature
+            client.readInputRegisters(1046, 1),   // Compressor frequency
+            client.readInputRegisters(2166, 1)    // Energy usage
 
         ]).then((results) => {
     
@@ -103,13 +105,15 @@ class s11556pcDevice extends Device {
             var temperature_heating_supply = results[4].response._body._valuesAsBuffer;
             var temperature_heating_return = results[5].response._body._valuesAsBuffer;
             var degree_minutes = results[6].response._body._valuesAsBuffer;
-            var temperature_hot_water = results[7].response._body._valuesAsBuffer;
-            var temperature_calculated_supply = results[8].response._body._valuesAsBuffer;
-            var heating_pump = results[9].response._body._valuesAsBuffer;
-            var source_pump = results[10].response._body._valuesAsBuffer;
-            var priority = results[11].response._body._valuesAsBuffer;
-            var rmu_s40_temperature = results[12].response._body._valuesAsBuffer;
-            var energy_usage = results[13].response._body._valuesAsBuffer;
+            var temperature_hot_water_top = results[7].response._body._valuesAsBuffer;
+            var temperature_hot_water_load = results[8].response._body._valuesAsBuffer;
+            var temperature_calculated_supply = results[9].response._body._valuesAsBuffer;
+            var heating_pump = results[10].response._body._valuesAsBuffer;
+            var source_pump = results[11].response._body._valuesAsBuffer;
+            var priority = results[12].response._body._valuesAsBuffer;
+            var rmu_s40_temperature = results[13].response._body._valuesAsBuffer;
+            var compressor_frequency = results[14].response._body._valuesAsBuffer;
+            var energy_usage = results[15].response._body._valuesAsBuffer;
     
             // Convert HEX to Decimal and divide by the scalefactor
             temperature_outside = temperature_outside.readInt16BE().toString() /10;                         // scale factor is 10
@@ -119,13 +123,15 @@ class s11556pcDevice extends Device {
             temperature_heating_supply = temperature_heating_supply.readInt16BE().toString() / 10;          // scale factor is 10
             temperature_heating_return = temperature_heating_return.readInt16BE().toString() / 10;          // scale factor is 10
             degree_minutes = degree_minutes.readInt16BE().toString() / 10;                                  // scale factor is 10
-            temperature_hot_water = temperature_hot_water.readInt16BE().toString() / 10;                    // scale factor is 10
+            temperature_hot_water_top = temperature_hot_water_top.readInt16BE().toString() / 10;            // scale factor is 10
+            temperature_hot_water_load = temperature_hot_water_load.readInt16BE().toString() / 10;          // scale factor is 10
             temperature_calculated_supply = temperature_calculated_supply.readInt16BE().toString() / 10;    // scale factor is 10
             heating_pump = heating_pump.readInt16BE().toString() / 1;                                       // scale factor is 1
             source_pump = source_pump.readInt16BE().toString() / 1;                                         // scale factor is 1
             priority = priority.readInt16BE().toString() / 1;                                               // scale factor is 1
             rmu_s40_temperature = rmu_s40_temperature.readInt16BE().toString() / 10;                        // scale factor is 10
-            energy_usage = energy_usage.readInt16BE().toString() / 1;                                               // scale factor is 1
+            compressor_frequency = compressor_frequency.readInt16BE().toString() / 1;                       // scale factor is 1
+            energy_usage = energy_usage.readInt16BE().toString() / 1;                                       // scale factor is 1
 
             // Translate priority mode to text
             if (priority == 10) {
@@ -155,11 +161,13 @@ class s11556pcDevice extends Device {
             this.setCapabilityValue('measure_temperature.heating_supply', temperature_heating_supply);
             this.setCapabilityValue('measure_temperature.heating_return', temperature_heating_return);
             this.setCapabilityValue('measure_temperature.degree_minutes', degree_minutes);
-            this.setCapabilityValue('measure_temperature.hot_water', temperature_hot_water);
+            this.setCapabilityValue('measure_temperature.hot_water_top', temperature_hot_water_top);
+            this.setCapabilityValue('measure_temperature.hot_water_load', temperature_hot_water_load);
             this.setCapabilityValue('measure_temperature.calculated_supply', temperature_calculated_supply);
             this.setCapabilityValue('measure_percentage.heating_pump', heating_pump);
             this.setCapabilityValue('measure_percentage.source_pump', source_pump);
             this.setCapabilityValue('measure_string.priority', priority);
+            this.setCapabilityValue('measure_string.compressor_frequency', compressor_frequency);
             this.setCapabilityValue('measure_power', energy_usage);
 
             // this.log('All values set')
